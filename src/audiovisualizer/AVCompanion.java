0c53b471c425d762b7547741ad45514bd037adce
Stjevvvvvvvvvvv <sddvissc.DeVisscher@UGent.be>
2018-06-10 22:57:48 +0200

    initial commit

---
+++ src/audiovisualizer/AVCompanion.java
@@ -0,0 +1,14 @@
+package audiovisualizer;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+
+public class AVCompanion {
+
+    public Pane pane;
+
+    public void initialize(){
+        Rectangle visualBlock = new Rectangle();
+    }
+
+}
